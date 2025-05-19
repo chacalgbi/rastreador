d4ccd05b9e6cd27345ce0e9d13d8f3d83be46958
Thomé Lucas <chacalgbi@hotmail.com>
2025-05-19 11:04:26 -0300

    Preencher automaticamente os campos

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -1,4 +1,6 @@
 class Detail < ApplicationRecord
+  before_validation :set_default_values
+
   def self.ransackable_associations(auth_object = nil)
     %w[device_id device_name model ignition rele_state url velo_max battery bat_bck horimetro odometro cercas satelites version imei bat_nivel signal_gps signal_gsm acc acc_virtual charge heartbeat obs status network params last_event_type last_user apn ip_and_port alert_whatsApp alert_telegram alert_email send_exit_cerca send_battery send_moving send_velo_max send_rele created_at updated_at]
   end
@@ -7,4 +9,13 @@ class Detail < ApplicationRecord
     ["acc", "acc_virtual", "alert_email", "alert_telegram", "alert_whatsApp", "apn", "bat_bck", "bat_nivel", "battery", "cercas", "charge", "created_at", "device_id", "device_name", "heartbeat", "horimetro", "id", "id_value", "ignition", "imei", "ip_and_port", "last_event_type", "last_rele_modified", "last_user", "model", "network", "obs", "odometro", "params", "rele_state", "satelites", "send_battery", "send_moving", "send_exit_cerca", "send_rele", "send_velo_max", "signal_gps", "signal_gsm", "status", "updated_at", "url", "velo_max", "version"]
   end
 
+  private
+
+  def set_default_values
+    self.model ||= "xt40"
+    self.ignition ||= "off"
+    self.rele_state ||= "off"
+    self.status ||= "online"
+    self.last_user ||= "System"
+  end
 end
