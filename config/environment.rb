7ca154a87f8130d87ab2d34f0ee553b1b6dbb635
Thomé Lucas <chacalgbi@hotmail.com>
2025-06-05 14:38:32 -0300

    Remover do enviromnt.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
 # Load the Rails application.
 require_relative "application"
-require Rails.root.join("app/jobs/send_command_job")
 
 # Initialize the Rails application.
 Rails.application.initialize!
