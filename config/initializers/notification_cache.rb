b5d8bc73acccc4f9b231ac9e3e54860a25f579a1
Thomé Lucas <chacalgbi@hotmail.com>
2025-06-21 14:49:23 -0300

    Alteração ao inicializar Notifications

--- config/initializers/notification_cache.rb
+++ config/initializers/notification_cache.rb
@@ -1,3 +1,11 @@
 Rails.application.config.after_initialize do
-  NotificationCacheService.load_notifications
+  begin
+    if ActiveRecord::Base.connection.table_exists?('notifications')
+      NotificationCacheService.load_notifications
+    else
+      Rails.logger.warn "Tabela notifications não existe ainda. Cache será carregado após migration."
+    end
+  rescue => e
+    Rails.logger.error "Erro ao carregar cache de notificações: #{e.message}"
+  end
 end
