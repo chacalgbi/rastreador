da26e3850bf8865a1c29905227f7840226011728
Thomé Lucas <chacalgbi@hotmail.com>
2025-09-30 07:25:16 -0300

    Add log no initialize

--- config/initializers/motocycle_job.rb
+++ config/initializers/motocycle_job.rb
@@ -2,6 +2,7 @@ Rails.application.config.after_initialize do
   if Rails.env.production?
     Thread.new do
       sleep 10
+      Rails.logger.info("\n\nIniciando SearchStoppedMotorcyclesJob recorrente...\n\n")
       SearchStoppedMotorcyclesJob.start_recurring
     end
   end
