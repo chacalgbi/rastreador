0e2405043563f076798dfc6d9c40482a2fb61a6f
Thomé Lucas <chacalgbi@hotmail.com>
2025-10-01 18:58:15 -0300

    Mudança de Icone de acordo o servidor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,17 @@ module ApplicationHelper
   def mobile_device?
     request.user_agent =~ /Mobile|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i
   end
+
+  def icon(server)
+    case server
+    when 'LOCAL'
+      'local'
+    when 'QA'
+      'qa'
+    when 'CTE'
+      'cte'
+    else
+      'default'
+    end
+  end
 end
